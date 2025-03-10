c9f5017c5042246349b98ff93ee7b9ff258a06ad
Letícia Heeren <leticia.tvsh@gmail.com>
2025-03-09 21:36:54 -0300

    adicionar suporte a números possíveis em cada célula

--- src/br/com/dio/model/Space.java
+++ src/br/com/dio/model/Space.java
@@ -1,17 +1,25 @@
 package br.com.dio.model;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Space {
 
     private Integer actual;
     private final int expected;
     private final boolean fixed;
-
+    private Set<Integer> possibleNumbers;
 
     public Space(final int expected, final boolean fixed) {
         this.expected = expected;
         this.fixed = fixed;
-        if (fixed){
+        if (fixed) {
             actual = expected;
+        } else {
+            this.possibleNumbers = new HashSet<>();
+            for (int i = 1; i <= 9; i++) {
+                possibleNumbers.add(i);
+            }
         }
     }
 
@@ -24,7 +32,7 @@ public class Space {
         this.actual = actual;
     }
 
-    public void clearSpace(){
+    public void clearSpace() {
         setActual(null);
     }
 
@@ -34,5 +42,17 @@ public class Space {
 
     public boolean isFixed() {
         return fixed;
+    }
+
+    public Set<Integer> getPossibleNumbers() {
+        return possibleNumbers;
+    }
+
+    public void removePossibleNumber(int number) {
+        possibleNumbers.remove(number);
+    }
+
+    public void addPossibleNumber(int number) {
+        possibleNumbers.add(number);
     }
 }
